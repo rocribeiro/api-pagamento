7d51945135b3a75291ae50f9c8c8eb5026275ee2
Rodrigo Ribeiro <rodrigo.ribeiro03@hotmail.com>
2020-01-23 23:12:12 -0300

    Ajustes

--- src/main/java/com/api/desafio/model/Client.java
+++ src/main/java/com/api/desafio/model/Client.java
@@ -1,14 +1,13 @@
 package com.api.desafio.model;
 
-import lombok.Getter;
-import lombok.Setter;
+
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 @Entity
-@Getter @Setter
+
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/api/desafio/model/Payment.java
+++ src/main/java/com/api/desafio/model/Payment.java
@@ -1,19 +1,17 @@
 package com.api.desafio.model;
 
-import lombok.Getter;
-import lombok.Setter;
+
 
 import javax.persistence.*;
 
 @Entity
-@Getter @Setter
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToOne(cascade= CascadeType.ALL, fetch=FetchType.EAGER)
     private Client client;
-    private Double Amount;
+    private Double amount;
     private String boletoNumber;
     private int Type;
     private String status;
@@ -22,4 +20,59 @@ public class Payment {
     @Transient
     private Card card;
 
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getBoletoNumber() {
+        return boletoNumber;
+    }
+
+    public void setBoletoNumber(String boletoNumber) {
+        this.boletoNumber = boletoNumber;
+    }
+
+    public int getType() {
+        return Type;
+    }
+
+    public void setType(int type) {
+        Type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Buyer getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(Buyer buyer) {
+        this.buyer = buyer;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
 }
